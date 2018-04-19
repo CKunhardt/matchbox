0831dbb233240f1aa5981da9eadc1366d1c4fc9e
Clinton Kunhardt <24also20@gmail.com>
2018-04-18 23:23:10 -0500

    Finished authentication

---
+++ MatchboxClient/iOS/Services/iOSLoginProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Foundation;
+using UIKit;
+
+using Microsoft.WindowsAzure.MobileServices;
+
+using Matchbox.Abstractions;
+
+namespace Matchbox.iOS.Services
+{
+    public class iOSLoginProvider : ILoginProvider
+    {
+        public async Task LoginAsync(MobileServiceClient client)
+        {
+            await client.LoginAsync(RootView, MobileServiceAuthenticationProvider.Google, "matchbox");
+        }
+
+        public UIViewController RootView => UIApplication.SharedApplication.KeyWindow.RootViewController;
+    }
+}
